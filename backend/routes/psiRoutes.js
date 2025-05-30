9245360c44917fc52120c42045720cf2272dbd4d
Sohel <shaikhsohel113441@gmail.com>
2025-05-30 23:40:26 +0530

    SS Added

--- backend/routes/psiRoutes.js
+++ backend/routes/psiRoutes.js
@@ -3,8 +3,7 @@ import { analyzeUrl , getAllReports } from '../controllers/psiController.js';
 
 const router = express.Router();
 
-// Route: GET /api/insights/analyze?url=https://example.com
-router.get('/', getAllReports); // GET /api/insights
+router.get('/', getAllReports);
 
 router.post('/analyze', analyzeUrl);
 
